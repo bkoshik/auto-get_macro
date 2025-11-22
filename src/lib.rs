376327b84fdb7715654be32387fb7e7d7b56531f
bkoshik <bakubakosh@gmail.com>
2025-11-22 12:07:47 +0500

    Add auto_get_derive

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Fields, Ident, Type};
+
+#[proc_macro_derive(AutoGet)]
+pub fn auto_get_derive(input: TokenStream) -> TokenStream {
+    let input: DeriveInput = parse_macro_input!(input as DeriveInput);
+    let name: Ident = input.ident;
+
+    let fields: Fields = if let syn::Data::Struct(data) = input.data {
+        data.fields
+    } else {
+        return TokenStream::new()
+    };
+
+    let getters = fields.iter().map(|f| {
+        let f_name: &Ident = f.ident.as_ref().unwrap();
+        let f_type: &Type = &f.ty;
+
+        quote! {
+            pub fn #f_name(&self) -> #f_type {
+                &self.#f_name
+            }
+        }
+    });
+
+    let expanded: TokenStream = quote! {
+        impl #name {
+            #(#getters)*
+        }
+    }.into();
+
+    TokenStream::from(expanded)
+}
